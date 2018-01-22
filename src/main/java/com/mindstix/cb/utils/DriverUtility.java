48e5b94f7c0f78d1f724d1a0051fd1955996393f
Kalpesh Chaudhari <kalpesh.chaudhari@mindstix.com>
2018-01-22 16:18:12 +0530

    Added saucelabs and browserstack integrations

--- src/main/java/com/mindstix/cb/utils/DriverUtility.java
+++ src/main/java/com/mindstix/cb/utils/DriverUtility.java
@@ -42,16 +42,30 @@ public final class DriverUtility {
 	}
 
 	/**
-	 * Method to load properties according to mentioned Browser Name and
-	 * Initialize Web Driver
+	 * Method to load properties according to mentioned Browser Name and Initialize
+	 * Web Driver
 	 * 
 	 * @param browserName
 	 *            -Name of Browser Mentioned in testng.xml
 	 */
 	public static WebDriver loadDriver(String browserName) {
 		InputStream driverInput = null;
+		WebDriver webDriver = null;
 		try {
-			WebDriver webDriver = initDriver(browserName);
+			switch (System.getProperty("env.platform")) {
+			case "local":
+				webDriver = initDriver(browserName);
+				break;
+			case "browserStack":
+				webDriver = initBrowserStack();
+				break;
+			case "sauceLabs":
+				webDriver = initSauceLabs();
+				break;
+			default:
+				webDriver = initDriver(browserName);
+				break;
+			}
 			webDriver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 			webDriver.manage().timeouts().setScriptTimeout(9000, TimeUnit.MILLISECONDS);
 			webDriver.manage().timeouts().pageLoadTimeout(10, TimeUnit.MINUTES);
@@ -73,8 +87,7 @@ public final class DriverUtility {
 	}
 
 	/**
-	 * To initialize Web Driver according to value of browserName provided in
-	 * .xml
+	 * To initialize Web Driver according to value of browserName provided in .xml
 	 * 
 	 * @param browserType
 	 *            Browser Name
@@ -167,4 +180,59 @@ public final class DriverUtility {
 		webDriver.manage().window().maximize();
 		return webDriver;
 	}
+
+	/**
+	 * To initialize Browser Stack Remote Web Driver
+	 * 
+	 * @return Web Driver
+	 */
+	private static WebDriver initBrowserStack() {
+		LOGGER.info("Inittializing browserstack");
+		WebDriver webDriver = null;
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		String username = System.getenv("BROWSERSTACK_USER");
+		String accessKey = System.getenv("BROWSERSTACK_ACCESSKEY");
+		String browserstackLocal = System.getenv("BROWSERSTACK_LOCAL");
+		String browserstackLocalIdentifier = System.getenv("BROWSERSTACK_LOCAL_IDENTIFIER");
+		String browser = System.getProperty("env.browser");
+		String os = System.getProperty("os.name");
+		os = os.split(" ", 2)[0];
+		capabilities.setCapability("browser", browser);
+		capabilities.setCapability("os", os);
+		capabilities.setCapability("browserstack.local", browserstackLocal);
+		capabilities.setCapability("browserstack.localIdentifier", browserstackLocalIdentifier);
+		try {
+			webDriver = new RemoteWebDriver(
+					new URL("https://" + username + ":" + accessKey + "@hub.browserstack.com/wd/hub"), capabilities);
+		} catch (MalformedURLException e) {
+			throw new RuntimeException(e.getMessage());
+		}
+		return webDriver;
+	}
+
+	/**
+	 * To initialize SauceLabs Remote Web Driver
+	 * 
+	 * @return Web Driver
+	 */
+	private static WebDriver initSauceLabs() {
+		LOGGER.info("Inittializing saucelabs");
+		WebDriver webDriver = null;
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		String username = System.getenv("SAUCELABS_USER");
+		String accessKey = System.getenv("SAUCELABS_ACCESSKEY");
+		String browser = System.getProperty("env.browser");
+		String os = System.getProperty("os.name");
+		capabilities.setCapability("browserName", browser);
+		capabilities.setCapability("platform", os);
+		try {
+			webDriver = new RemoteWebDriver(
+					new URL("https://" + username + ":" + accessKey + "@ondemand.saucelabs.com:443/wd/hub"),
+					capabilities);
+		} catch (MalformedURLException e) {
+			throw new RuntimeException(e.getMessage());
+		}
+		return webDriver;
+	}
+
 }
